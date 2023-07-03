412234ac68afca665e0a0e6db80360a27fdbd3c3
alinaderi <malinaderi1@outlook.com>
2023-07-04 01:44:16 +0330

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  devtool: "hidden-source-map",
   resolve: {
     fallback: {
       querystring: require.resolve("querystring-es3"),
